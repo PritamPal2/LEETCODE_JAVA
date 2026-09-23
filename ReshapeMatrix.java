de07daf8ec7630612aa8650dad4baad1960a2794
PritamPal2 <222634426+PritamPal2@users.noreply.github.com>
2026-09-23 21:54:57 +0530

    Leet Code 566

--- ReshapeMatrix.java
+++ ReshapeMatrix.java
@@ -8,19 +8,19 @@ class ReshapeMatrix {
         int a=0;
         int b=0;
         for(int i=0;i< rows * cols;i++) {
-            result[a][b] = mat[i/cols][i%cols];
-            b++;
-            if(b == c) {
-                a++;
-                b=0;
-            }
+            result[i/c][i%c] = mat[i/cols][i%cols];
+            // b++;
+            // if(b == c) {
+            //     a++;
+            //     b=0;
+            // }
         }
         return result;
     }
 
     public static void main(String[] args) {
-        // int[][] mat = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20}};
-        int[][] mat = {{1,2},{3,4}};
+        int[][] mat = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20}};
+        // int[][] mat = {{1,2},{3,4}};
         int r = 2;
         int c = 10;
 
