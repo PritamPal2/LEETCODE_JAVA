7776e1b23f9a2f3581ffc79ebfd7555d1c1f39e9
PritamPal2 <222634426+PritamPal2@users.noreply.github.com>
2026-09-27 00:41:22 +0530

    Leet Code 3898

---
+++ DegreeOfEachVertex.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class DegreeOfEachVertex {
+    public static int[] findDegrees(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] rowSum = new int[rows];
+
+        for(int i=0;i<rows;i++) {
+            // int count = 0;
+            for(int j=0;j<cols;j++) {
+                if(matrix[i][j] == 1) {
+                    rowSum[i] += 1;
+                    // count++;
+                }
+            }
+            // rowSum[i] = count;
+            
+        }
+        return rowSum;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{0,1,1},{1,0,1},{1,1,0}};
+
+        System.err.println(Arrays.toString(findDegrees(matrix)));
+    }
+}
